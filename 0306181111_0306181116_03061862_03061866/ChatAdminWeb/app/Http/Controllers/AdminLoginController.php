7ae886364f39f785ad92c4619e7baabf8814f11e
ThanhTai2132000 <74943218+ThanhTai2132000@users.noreply.github.com>
2020-12-14 22:57:48 +0700

    Login

---
+++ 0306181111_0306181116_03061862_03061866/ChatAdminWeb/app/Http/Controllers/AdminLoginController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+class AdminLoginController extends Controller
+{
+    public function getcheck()
+    {
+        return view('auth.login');
+    }
+
+
+    public function check(Request $request)
+    {
+        $data = [
+            'name' => $request->name,
+            'password' => $request->password,
+        ];
+
+        if (Auth::attempt($data)) {
+            return view('welcome');
+        } else {
+            return view('auth.login')
+        }
+    }
+
+}
--- 0306181111_0306181116_03061862_03061866/ChatAdminWeb/routes/web.php
+++ 0306181111_0306181116_03061862_03061866/ChatAdminWeb/routes/web.php
@@ -15,9 +15,14 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('logintest', 'AdminLoginController@getcheck');
+Route::post('logintest', 'AdminLoginController@check');
+
 Route::get('/user', "UserController@index")->name('user.index');
 Route::post('/user', "UserController@store")->name('user.store');
 Route::get('/user/create', "UserController@create")->name('user.create');
 Route::get('/user/{id}/edit', "UserController@edit")->name('user.edit');
 Route::get('/user/{id}', "UserController@show")->name('user.show');
 Route::patch('/user/{id}', "UserController@update")->name('user.update');
+
